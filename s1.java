f9392b9b331e318f22f18321cc089255587f458a
logambika <logambika611@gmail.com>
2016-08-09 15:43:40 +0530

    Update s1.java

--- s1.java
+++ s1.java
@@ -1,7 +1,15 @@
-int a;
-Scanner s=new Scanner(System.in)
-a=s.next();
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+int a=5;
 if(a%2==0)
 System.out.println("even");
 else
 System.out.println("odd");
+	}
+}
